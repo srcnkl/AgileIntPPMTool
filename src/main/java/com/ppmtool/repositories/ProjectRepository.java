4143588dfc724329e687aea01f834007ca63db0e
srcnkl <sercankaliu@gmail.com>
2020-04-19 19:34:22 +0300

    Entity and project end-point is created

---
+++ src/main/java/com/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.ppmtool.repositories;
+
+import com.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
